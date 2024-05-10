8d30f13545cfed87dcee24c40c88b8de8585b11d
Samuel Ampah <ampahsamuel99@gmail.com>
2024-05-10 01:34:34 +0000

    worked with methods in class

--- data/cart-class.js
+++ data/cart-class.js
@@ -110,9 +110,3 @@ class Cart {
   const cart = new Cart('cart-oop');
   const businessCart = new Cart('cart-business');
 
-
-
-  console.log(cart);
-  console.log(businessCart)
-  console.log(businessCart instanceof Cart);
-  
--- data/cart.js
+++ data/cart.js
@@ -36,7 +36,7 @@ export function addToCart(productId) {
   const quantitySelector = document.querySelector(
     `.js-quantity-selector-${productId}`
   );
-  const quantity = Number(quantitySelector);
+  const quantity = Number(quantitySelector.value);
 
   if (matchingItem) {
     matchingItem.quantity += quantity;  
